f9f336e6d9c60ead69cc9ce5b5149684b3e22b01
milos012 <milos441@gmail.com>
2023-05-19 21:26:46 +0200

    kreiranje baze podataka

--- backend/src/main/java/ftn/isamrs/backend/models/Role.java
+++ backend/src/main/java/ftn/isamrs/backend/models/Role.java
@@ -1,49 +1,49 @@
-package ftn.isamrs.backend.models;
+// package ftn.isamrs.backend.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.*;
+// import javax.persistence.*;
 
-import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.GrantedAuthority;
 
-// POJO koji implementira Spring Security GrantedAuthority kojim se mogu definisati role u aplikaciji
-@Entity
-@Table(name="ROLE")
-public class Role implements GrantedAuthority{
+// // POJO koji implementira Spring Security GrantedAuthority kojim se mogu definisati role u aplikaciji
+// @Entity
+// @Table(name="ROLE")
+// public class Role implements GrantedAuthority{
 
-    private static final long serialVersionUID = 1L;
+//     private static final long serialVersionUID = 1L;
 
-    @Id
-    @Column(name="id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+//     @Id
+//     @Column(name="id")
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     Long id;
 
-    @Column(name="name")
-    String name;
+//     @Column(name="name")
+//     String name;
 
-    @JsonIgnore
-    @Override
-    public String getAuthority() {
-        return name;
-    }
+//     @JsonIgnore
+//     @Override
+//     public String getAuthority() {
+//         return name;
+//     }
     
-    public void setName(String name) {
-        this.name = name;
-    }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    @JsonIgnore
-    public Long getId() {
-        return id;
-    }
+//     @JsonIgnore
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
 
     
-}
+// }
