b8f27e7b8e5e64bca6504c883221790dff5a7758
milos012 <milos441@gmail.com>
2023-05-29 22:08:46 +0200

    Login logic

--- frontend/src/components/registration/SignIn.jsx
+++ frontend/src/components/registration/SignIn.jsx
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -12,18 +10,45 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const navigate = useNavigate();
+
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
       password: data.get('password'),
     });
+
+    try {
+        const response = axios.post("http://localhost:9000/api/login",
+          JSON.stringify({ email, password }),
+          {
+              headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+              withCredentials: true
+          });
+        const accessToken = (await response)?.data?.accessToken;
+        const roles = (await response)?.data?.roles;
+        setAuth({ email, password, roles, accessToken });
+        setEmail('');
+        setPassword('');
+      } catch (error) {
+        console.log("Greska prilikom logina")
+      }
+  
+      navigate('/')
+
   };
 
   return (
